634b2fe71f8d1ee9511e14f2030f3076d0797f67
Badsauce <Ninjazoby@gmail.com>
2015-02-12 21:15:18 +0000

    it works now, ruby data structures have silly syntax sometimes

--- app.rb
+++ app.rb
@@ -1,9 +1,15 @@
 require 'sinatra'
 require 'holidapi'
+require 'date'
 
 class MyWebApp < Sinatra::Base
   get '/' do
-    @holidays = HolidApi.get
+    @holidays = HolidApi.get(month: 2)
+    erb :holidays
+  end
+
+  get '/myBirthday' do
+    @holidays = HolidApi.get(month: 2, year: 1993)
     erb :holidays
   end
 end
