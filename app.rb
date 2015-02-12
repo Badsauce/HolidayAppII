667cab4d58782a187c31bbb35bde81e9149347dd
Badsauce <Ninjazoby@gmail.com>
2015-02-12 22:43:31 +0000

    welcome to app town

--- app.rb
+++ app.rb
@@ -4,12 +4,11 @@ require 'date'
 
 class MyWebApp < Sinatra::Base
   get '/' do
-    @holidays = HolidApi.get(month: 2)
-    erb :holidays
-  end
+    @month = params['month'] ? params['month'] : 1
+    @year = params['year'] ? params['year'] : 2015
+    @day = params['day'] ? params['day'] : 1
 
-  get '/myBirthday' do
-    @holidays = HolidApi.get(month: 2, year: 1993)
+    @holidays = HolidApi.get( month: @month, year: @year, day: @day)
     erb :holidays
   end
 end
