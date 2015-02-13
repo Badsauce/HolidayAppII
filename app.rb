92651e54c2f70b5784868af8d11a7b0af24e5ec0
Badsauce <Ninjazoby@gmail.com>
2015-02-13 12:56:49 -0500

    Form now populates with entered values

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ require 'date'
 class MyWebApp < Sinatra::Base
   get '/' do
     @month = params['month'] ? params['month'] : 1
-    @year = params['year'] ? params['year'] : 2015
+    @year = params['year'] ? params['year'] : 2000
     @day = params['day'] ? params['day'] : 1
 
     @holidays = HolidApi.get( month: @month, year: @year, day: @day)
