386f5298273a6cb534633b0945297ebc08e58f74
Badsauce <Ninjazoby@gmail.com>
2015-02-13 18:05:37 -0500

    fixed rack bug, fixed empty form bug

--- app.rb
+++ app.rb
@@ -4,9 +4,9 @@ require 'date'
 
 class MyWebApp < Sinatra::Base
   get '/' do
-    @month = params['month'] ? params['month'] : 1
-    @year = params['year'] ? params['year'] : 2000
-    @day = params['day'] ? params['day'] : 1
+    @month = params['month'] != '' ? params['month'] : 1
+    @year = params['year'] != '' ? params['year'] : 2000
+    @day = params['day'] != '' ? params['day'] : 1
 
     @holidays = HolidApi.get( month: @month, year: @year, day: @day)
     erb :holidays
